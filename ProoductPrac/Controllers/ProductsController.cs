2b7b40e46738d316515c6dc4dd621e3bf0cd4e02
DEepank yadav <vikkyy703@gmail.com>
2025-04-05 10:40:14 +0530

    Initial commit

---
+++ ProoductPrac/Controllers/ProductsController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProoductPrac.Model;
+using ProoductPrac.Services;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ProoductPrac.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductServices _productService;
+
+        public ProductsController(ProductServices productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Product>> GetAll()
+        {
+            return Ok(_productService.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetById(int id)
+        {
+            var product = _productService.GetById(id);
+            if (product == null) return NotFound();
+
+            return Ok(product);
+        }
+
+        
+
+        [HttpPost]
+        public ActionResult<Product> Create([FromBody] Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+
+            _productService.Add(product);
+            return CreatedAtAction(nameof(GetById), new { id = product.Id },product);
+        }
+
+
+        [HttpPut("{id}")]
+        public ActionResult Update(int id, [FromBody] Product product)
+        {
+            if (id != product.Id) return BadRequest();
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            if (!_productService.Update(id, product)) return NotFound();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            if (!_productService.Delete(id)) return NotFound();
+            return NoContent();
+        }
+
+    }
+}
---
+++ ProoductPrac/Model/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProoductPrac.Model
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage ="Name is required")]
+        [StringLength(50,MinimumLength =2,ErrorMessage ="name must be between 2 and 50 characters")]
+        public string Name { get; set; }
+
+        [Range(0.01,10000.00, ErrorMessage ="price must be between 0.01 and 10000.00")]
+        public decimal Price { get; set; }
+    }
+}
